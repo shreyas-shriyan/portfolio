4695d461d953b389ff655ea9fbe83e71eaf6794c
shreyas-shriyan <shreyshriyan@gmail.com>
2026-02-26 11:42:04 +0530

    style: basic new design

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-   <h1>Shreyas Shriyan</h1>
-   <p>Software Developer</p>
-   <p>Bangalore, India</p>
-  </div>
-`;
